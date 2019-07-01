0fc58afe163250ada2d2f2b42a6f3605f46b9030
CGR Lab 307 <cgrlab307@senacmsedu.local>
2019-07-01 14:27:27 -0400

    validações

---
+++ ProjetoPET/Migrations/20190628183558_asd.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjetoPET.Migrations
+{
+    public partial class asd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DataHora",
+                table: "Eventos",
+                newName: "DataHoraInicio");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataHoraFim",
+                table: "Eventos",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DataHoraFim",
+                table: "Eventos");
+
+            migrationBuilder.RenameColumn(
+                name: "DataHoraInicio",
+                table: "Eventos",
+                newName: "DataHora");
+        }
+    }
+}
