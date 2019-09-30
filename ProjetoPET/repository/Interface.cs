433cd8e3c57c80899601a55485bbc2004c702b5d
Fabrica <Fabrica@CGR-24027.senacmsedu.local>
2019-09-30 17:32:01 -0400

    Tela de anuncio de animais

--- ProjetoPET/repository/Interface.cs
+++ ProjetoPET/repository/Interface.cs
@@ -5,15 +5,15 @@ using System.Threading.Tasks;
 
 namespace ProjetoPET.repository
 {
-    
-       public interface IGenericRepository<TEntity> where TEntity : class
+
+    public interface IGenericRepository<TEntity> where TEntity : class
     {
-            Task<IList<TEntity>> ListarTodos();
-            Task Inserir(TEntity e);
-            Task<TEntity> BuscarPorId(int id);
-            Task Editar(int id, TEntity e);
-            Task Excluir(int id);
-            Task<bool> Existe(int id);
-        }
+        Task<IList<TEntity>> ListarTodos();
+        Task Inserir(TEntity e);
+        Task<TEntity> BuscarPorId(int id);
+        Task Editar(int id, TEntity e);
+        Task Excluir(int id);
+        Task<bool> Existe(int id);
+    }
 }
 
