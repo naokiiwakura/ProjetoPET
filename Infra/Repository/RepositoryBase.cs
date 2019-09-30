f99994a5872bf08bfe0e20b5da6cc3ca2129b029
Laboratório 001 <lab001@senacmsedu.local>
2019-09-30 15:36:27 -0400

    adicao do construtor do bancocontext

--- Infra/Repository/RepositoryBase.cs
+++ Infra/Repository/RepositoryBase.cs
@@ -1,10 +1,35 @@
-﻿using System;
+﻿using Domain.Intefaces;
 using System.Collections.Generic;
-using System.Text;
+using System.Threading.Tasks;
 
 namespace Infra.Repository
 {
-    class RepositoryBase
+    public class RepositoryBase<TEntity> : IRepositoryBase<TEntity> where TEntity : class
     {
+        //BancoContext _db = new BancoContext();
+
+        public async Task<TEntity> GetById(int id) =>
+             await _db.Set<TEntity>().FindAsync(id);
+
+        public async Task<IEnumerable<TEntity>> GetAll() =>
+             await _db.Set<TEntity>().ToListAsync();
+
+        public async Task Update(TEntity e)
+        {
+            _db.Set<TEntity>().Update(e);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task Delete(TEntity e)
+        {
+            _db.Set<TEntity>().Remove(e);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task Add(TEntity e)
+        {
+            await _db.AddAsync(e);
+            await _db.SaveChangesAsync();
+        }
     }
 }
