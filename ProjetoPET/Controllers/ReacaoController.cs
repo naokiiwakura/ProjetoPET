3e596c0ee6b5853e8754a803a3c6d1a06b07b605
Laboratório 001 <lab001@senacmsedu.local>
2019-09-30 17:43:23 -0400

    Projeto em andamneto

---
+++ ProjetoPET/Controllers/ReacaoController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProjetoPET.ViewModel;
+
+namespace ProjetoPET.Controllers
+{
+    public class ReacaoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(ReacaoViewModel reacaos)
+        {
+            return View();
+        }
+    }
+}
---
+++ ProjetoPET/Models/Reacao.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoPET.Models
+{
+    public class Reacao
+    {
+        public string Nome { get; set; }
+        public string Comentario { get; set; }
+
+    }
+}
