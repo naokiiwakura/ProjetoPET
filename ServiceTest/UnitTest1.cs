0a3aad5fdce133565bc8fe0ef1b2d0231bae966e
Ana Paula Chaves <annachaves00@hotmail.com>
2019-10-02 14:42:57 -0400

    testServices

---
+++ ServiceTest/UnitTest1.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+
+namespace ServiceTest
+{
+    public class UnitTest1
+    {
+        #region Mock
+
+        #endregion
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
